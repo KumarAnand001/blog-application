004c893da3a9a2746157a083461bd8917d587e4d
KumarAnand001 <anandkumarjnu9@gmail.com>
2024-04-10 20:26:35 +0530

    added test cases

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from account.serializers import RegisterSerializer
 
-# Create your tests here.
+from rest_framework import status
+import json
+
+class RegisterSerializerTest(TestCase):
+    def test_valid_registration(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        serializer = RegisterSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'johndoe')
+
+    def test_invalid_registration_username_taken(self):
+
+        User.objects.create(username='johndoe')
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        serializer = RegisterSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+
+class RegisterViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_valid_data(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/account/register/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_invalid_data(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe', 
+            'password': 'password123'
+        }
+        response = self.client.post('/api/account/register/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from blog.models import Post
 
-# Create your tests here.
+from rest_framework import status
+from django.contrib.auth.models import User
+import json
+
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(title='Test Post', author_id=1, body='This is a test post.')
+
+    def test_title_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_author_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field('author').verbose_name
+        self.assertEqual(field_label, 'author')
+
+    def test_body_max_length(self):
+        post = Post.objects.get(id=1)
+        max_length = post._meta.get_field('body').max_length
+        self.assertEqual(max_length, 1000)
+
+
+class ListPostViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_list_posts(self):
+        response = self.client.get('/api/blog/list-post/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class PostViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_post_creation(self):
+        self.client.login(username='testuser', password='testpassword')
+        data = {'title': 'New Post', 'body': 'This is a new post.'}
+        response = self.client.post('/api/blog/post/', data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
